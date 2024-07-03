b6dfc90505cfbff61cadf021bbe526ba70cdf21c
Дмитрий Левыкин <dmlegasy@dmail.com>
2024-07-04 01:09:21 +0300

    Новая индексация

--- task_1.py
+++ task_1.py
@@ -6,9 +6,13 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 
-new_data = pd.DataFrame({'robot' : [],
-                         'human' : []})
+new_data = pd.DataFrame({'robot' : [0] * len(lst),
+                         'human' : [0] * len(lst)})
 
-new_data['human'] = (data['whoAmI'] == 'human').astype(int)
-new_data['robot'] = (data['whoAmI'] == 'robot').astype(int)
+new_data.index = range(1, len(lst) + 1)
+new_data.index.name = 'Номер'
+
+new_data['human'] = (data['whoAmI'] == 'human').astype(int).values
+new_data['robot'] = (data['whoAmI'] == 'robot').astype(int).values
+
 print(new_data)
